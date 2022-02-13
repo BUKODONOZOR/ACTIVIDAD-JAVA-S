55c1d1532323393b0a87a2e524187a5ad92ccf4a
BUKODONOZOR <90481288+BUKODONOZOR@users.noreply.github.com>
2022-02-13 18:58:05 -0500

    Segundo comentario
    Aquí declare los atributos básicos del juego en cuanto a los movimientos , también las propiedades como la altura y el ancho . 
    Después di las propiedades iniciales del juego .
    Y La función que detiene la ejecución del juego  , cambiando el color a rojo ;

---
+++ main.js
@@ -0,0 +1,38 @@
+var game = function(){
+  let time = 50;
+  let movement = 20;
+  let movementBar = 20;
+  let width = document.documentElement.clientWidth - movement;
+  let height = document.documentElement.clientHeight - movement;
+  let controlGame;
+  let player1;
+  let player2;
+
+  function start(){
+      init();
+      controlGame = setInterval(play, time);
+  }
+
+  function init(){
+      ball.style.left = 0;
+      ball.state = 1;
+      ball.direction = 1; // right 1, left 2
+      player1 = new Object();
+      player2 = new Object();
+      player1.keyPress = false;
+      player1.keyCode = null;
+      player2.keyPress = false;
+      player2.keyCode = null;
+  }
+
+  function stop(){
+      clearInterval(controlGame);
+      document.body.style.background = "#f00";
+  }
+
+  function play(){
+      moveBall();
+      moveBar();
+      checkIfLost();
+  }
+}
